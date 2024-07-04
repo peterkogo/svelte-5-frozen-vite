cc84d08916c7ddd86984163455a381627be007c4
peterkogo <peter@xyflow.com>
2024-07-04 12:53:24 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+  import Component from "./Component.svelte";
+
+  function generateStuff(num: Number) {
+    return Array(num)
+      .fill(0)
+      .map((_, i) => ({
+        id: `${i}`,
+        position: {
+          x: Math.random() * 500,
+          y: Math.random() * 500,
+        },
+      }));
+  }
+
+  const partialAmount = 10;
+  const numberOfElements = 100;
+
+  let partial = $state(true);
+  let stuff = $state(generateStuff(numberOfElements));
+  let frozenStuff = $state.frozen($state.snapshot(stuff));
+
+  function changeDeepStuff() {
+    stuff.forEach((thing, i) => {
+      if (!partial || (partial && i < partialAmount)) {
+        thing.position.x = Math.random() * 500;
+        thing.position.y = Math.random() * 500;
+      }
+    });
+  }
+
+  function changeFrozenStuff() {
+    frozenStuff = frozenStuff.map((thing, i) => {
+      if (!partial || (partial && i < partialAmount)) {
+        return {
+          ...thing,
+          position: {
+            x: Math.random() * 500,
+            y: Math.random() * 500,
+          },
+        };
+      }
+      return thing;
+    });
+  }
+</script>
+
+{#each stuff as thing (thing.id)}
+  <Component {thing} color="purple" />
+{/each}
+{#each frozenStuff as thing (thing.id)}
+  <Component {thing} color="purple" />
+{/each}
+
+<button onmousedown={changeDeepStuff}> change deep stuff </button>
+
+<button onmousedown={changeFrozenStuff}> change frozen stuff </button>
+
+<label>
+  <input type="checkbox" bind:checked={partial} />
+  Partial Updates?
+</label>
---
+++ src/Component.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  let {
+    thing,
+    color,
+  }: {
+    thing: { id: string; position: { x: number; y: number } };
+    color: string;
+  } = $props();
+
+  let pos = $derived({ x: thing.position.x + 50, y: thing.position.y + 50 });
+
+  $effect(() => {
+    thing.position.x;
+    console.log("Updated " + thing.id);
+  });
+</script>
+
+<div
+  style:transform={`translate(${pos.x}px, ${pos.y}px)`}
+  style:background={color}
+></div>
+
+<style>
+  div {
+    position: absolute;
+    width: 5px;
+    height: 5px;
+    background: red;
+  }
+</style>
